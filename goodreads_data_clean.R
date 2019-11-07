70da43f113ff14dd10f21f653dd19ffbc7343187
dragongusto <56481432+dragongusto@users.noreply.github.com>
2019-11-07 08:18:26 -0500

    Added block to pull out stop words
    code takes all the words out of the reviews and lists them as individual words.  then removes stop words (common words)

--- goodreads_data_clean.R
+++ goodreads_data_clean.R
@@ -4,6 +4,7 @@ library(stringr)
 library(tidytext)
 library(dplyr)
 library(xlsx)
+library(ggplot2)
 
 good_reads <- read_excel("/Users/johnaugusterferiii/Documents/VCU Grad School/DAPT 2019-2020/Fall Semester/Text Mining/group project/all_data.xlsx")
 good_reads_clean <- good_reads[,c("book_title", "Book_series", "book_rating", "book_author", "genre", "reviewer_name", "review", "ID")]
@@ -21,12 +22,43 @@ good_reads_copy <- good_reads_copy[,c(1:5, 9, 6:8)]
 
 good_reads_test <- good_reads_copy[!(is.na(good_reads_copy$review)),]
 
-write_xlsx(good_reads_test, "/Users/johnaugusterferiii/Documents/VCU Grad School/DAPT 2019-2020/Fall Semester/Text Mining/group project/good_reads_write_underscore.xlsx", col_names = TRUE)
-write.xlsx(good_reads_test, "/Users/johnaugusterferiii/Documents/VCU Grad School/DAPT 2019-2020/Fall Semester/Text Mining/group project/good_reads_write_period.xlsx", col.names = TRUE)
+#write_xlsx(good_reads_test, "/Users/johnaugusterferiii/Documents/VCU Grad School/DAPT 2019-2020/Fall Semester/Text Mining/group project/good_reads_write_underscore.xlsx", col_names = TRUE)
+#write.xlsx(good_reads_test, "/Users/johnaugusterferiii/Documents/VCU Grad School/DAPT 2019-2020/Fall Semester/Text Mining/group project/good_reads_write_period.xlsx", col.names = TRUE)
 
 good_reads_copy %>%
   unnest_tokens(word, good_reads_copy$review)
 
-text_df <- tibble(line = 1:4, text = good_reads_copy$review)
+text_df <- tibble(text = good_reads_copy$review)
 
 text_df
+
+review_words <- text_df %>%
+  unnest_tokens(word, text)
+
+data(stop_words)
+
+review_words <- review_words %>%
+  anti_join(stop_words)
+
+review_words %>%
+  count(word, sort = TRUE)
+
+
+#Graphs the frequency of words.
+#the filter(n > 8000) means only the words that appear more than 8,000 times.
+review_words %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 8000) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
+
+
+
+
+
+
+
+
